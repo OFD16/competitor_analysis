af1954dc3325455ac0cad9ed555ce1300b2cae2e
Ömer Faruk Demirsoy <omerdmrsy@gmail.com>
2023-06-27 17:22:38 +0300

    fix product type

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,28 +1,31 @@
 class Product {
-  late final String? title;
-  late final String? description;
-  late final String? price;
-  late final String? discountPrice;
-  late final String? discountRate;
-  late final String? imageUrl;
-  late final String? shopOwnerName;
-  late final String? shopUrl;
-  late final String? shopName;
-  late final String? productCommentCount;
-  late final String? shopCommentCount;
-  late final List<String?> reviews;
+  String? id;
+  String? title;
+  String? description;
+  String? price;
+  String? discountPrice;
+  String? discountRate;
+  String? imageUrl;
+  String? shopOwnerName;
+  String? shopUrl;
+  String? shopName;
+  String? productCommentCount;
+  String? shopCommentCount;
+  List<String?> reviews;
 
-  Product(
-      this.title,
-      this.description,
-      this.price,
-      this.discountPrice,
-      this.discountRate,
-      this.imageUrl,
-      this.shopOwnerName,
-      this.shopUrl,
-      this.shopName,
-      this.shopCommentCount,
-      this.productCommentCount,
-      this.reviews);
+  Product({
+    this.id,
+    this.title,
+    this.description,
+    this.price,
+    this.discountPrice,
+    this.discountRate,
+    this.imageUrl,
+    this.shopOwnerName,
+    this.shopUrl,
+    this.shopName,
+    this.productCommentCount,
+    this.shopCommentCount,
+    this.reviews = const [],
+  });
 }
